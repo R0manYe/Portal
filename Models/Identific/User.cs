b0f39b44ae0a28f56056dd835f12f3892543812f
Roman <roman_y77@mail.ru>
2022-08-11 15:15:10 +0700

    Добавьте файлы проекта.

---
+++ Models/Identific/User.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Portal.Models.Identific
+{
+    public class User : IdentityUser
+    {
+        public int Year { get; set; }
+    }
+
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public int Year { get; set; }
+    }
+    public class CreateUserViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Year { get; set; }
+    }
+    public class ChangePasswordViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string NewPassword { get; set; }
+    }
+    public class IdentityRoles
+    {
+        public virtual IKey Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string NormalizedName { get; set; }
+        public virtual string ConcurrencyStamp { get; set; }
+    }
+    public class CustomPasswordValidator : IPasswordValidator<User>
+    {
+        public int RequiredLength { get; set; } // минимальная длина
+
+        public CustomPasswordValidator(int length)
+        {
+            RequiredLength = length;
+        }
+
+        public Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user, string password)
+        {
+            List<IdentityError> errors = new List<IdentityError>();
+
+            if (String.IsNullOrEmpty(password) || password.Length < RequiredLength)
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = $"Минимальная длина пароля равна {RequiredLength}"
+                });
+            }
+            string pattern = "^[0-9]+$";
+
+            if (!Regex.IsMatch(password, pattern))
+            {
+                errors.Add(new IdentityError
+                {
+                    Description = "Пароль должен состоять только из цифр"
+                });
+            }
+            return Task.FromResult(errors.Count == 0 ?
+                IdentityResult.Success : IdentityResult.Failed(errors.ToArray()));
+        }
+    }
+}
