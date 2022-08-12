377b3039aa345bff2f63189385251904f3889890
Roman <roman_y77@mail.ru>
2022-08-12 14:48:09 +0700

    Добавлен оранжевый дизайн

--- Startup.cs
+++ Startup.cs
@@ -68,7 +68,7 @@ namespace Portal
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index1}/{id?}");
             });
         }
     }
